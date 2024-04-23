e45fbb0c5cdd4b1d734ce86b50e4ce4543409157
Dawstin Waters <dawstinw4@gmail.com>
2024-04-23 06:37:53 -0700

    Documented Painting class

--- ts/main.ts
+++ ts/main.ts
@@ -2,10 +2,29 @@
  *  Represents an individual painting that can be purchased
  */
 class Painting {
+    /**
+     * The 5 digit inventory number
+     */
     inventorynumbers : string;
+
+    /**
+     * The 5 digit global ID
+     */
     globalIDs : string;
+
+    /**
+     * The name of the artwork archive
+     */
     artworkarchive : string;
+
+    /**
+     * The retail price of the painting
+     */
     price : number;
+
+    /**
+     * The title of the painting
+     */
     title : string;
 }
 
