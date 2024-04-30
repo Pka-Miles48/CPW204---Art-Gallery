9af8a94ed36433fd32e65a8a47a82b57456e9bb8
Dawstin Waters <dawstinw4@gmail.com>
2024-04-30 04:55:36 -0700

    Creating function stubs for the add painting form

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ myPainting.artworkarchive = "Schedule Feature";
 myPainting.price = 28.99;
 myPainting.title = "The Art of Programming for Novices";
 console.log(myPainting);
+window.onload = function () {
+    let addPaintingBtn = document.querySelector("#add-painting");
+    addPaintingBtn.onclick = processPainting;
+};
+function processPainting() {
+    alert("processPainting was called");
+}
--- ts/main.ts
+++ ts/main.ts
@@ -37,3 +37,38 @@ myPainting.price = 28.99;
 myPainting.title = "The Art of Programming for Novices"
 
 console.log(myPainting);
+
+
+window.onload = function() {
+    // set up button click for add painting form
+    let addPaintingBtn = document.querySelector("#add-painting") as HTMLButtonElement;
+    addPaintingBtn.onclick = processPainting;
+}
+
+function processPainting() {
+    console.log("processPainting was called")
+
+    let userPainting = getPainting();
+    if (userPainting !=null) {
+        addPainting(userPainting);
+    }
+}
+
+/**
+ * This function will retrieve all the painting
+ * data from the HTML page. If all data is valid
+ * a painting project will be returned. If any data
+ * is invalid, null will be returned.
+ */
+function getPainting():Painting {
+    
+}
+
+/**
+ * Adds a Painting object to web storage. Assumes
+ * all data is valid
+ * @param b The Painting containing valid data to be added
+ */
+function addPainting(b:Painting):void{
+
+}
